8caaaee9a544e6124f17e8b5febc794fa115e713
amberleyvs <amberley.vidya41@ui.ac.id>
2026-03-10 11:22:53 +0700

    [GREEN] Implement Payment model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Payment {
+
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+    private Order order;
+
+    public Payment(Order order, String method, Map<String, String> paymentData) {
+        this.id = UUID.randomUUID().toString();
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+    }
+}
